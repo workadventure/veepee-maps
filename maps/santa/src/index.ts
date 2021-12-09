eadea3a6bf7c4953f9769828e9b5b851b0007112
Hugo Averty <hugoaverty@gmail.com>
2021-12-08 20:30:36 -0400

    Update index.ts

--- maps/santa/src/index.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
-
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
-
-// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
